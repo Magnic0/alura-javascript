b1890bd3b405182dbb66064e71f9b3a3c8d126c5
Nicolas <nicolsoliverg@gmail.com>
2022-03-26 20:22:24 -0300

    Creating first javascript file

---
+++ first-program.js
@@ -0,0 +1,12 @@
+// Primeiro programa .js da série de cursos Javascript
+
+console.log("Hello world!");
+
+const age = 26;
+
+console.log(age);
+console.log(age * 78);
+
+const ageSum = age + 2;
+
+console.log(ageSum);
